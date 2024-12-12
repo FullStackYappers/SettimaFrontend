927778afaec5c34da3d856ffb0bc57a475105514
ameleroparra <ameleroparra@gmail.de>
2024-12-12 21:31:51 +0100

    LoginPage work

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,31 +4,31 @@ import nameWhite from "../assets/nameWhite.svg";
 
 const LoginPage = () => {
     return (
-        <div className="grid-container max-w-full max-h-full rounded-custom m-5 bg-accent">
+        <div className="grid-container max-w-full max-h-full rounded-custom bg-accent">
 
             <div className="logo">
                 <img src={nameWhite} alt="Settima Logo" className="w-40 h-auto"/>
             </div>
 
-            <div className="usernameInput">
+            <div className="usernameInput m-7">
                 <h1 className="m0 font-outfit text-primary font-medium">Username / Email</h1>
-                <input type="text" className="w-75% bg-bgcolor rounded-custom" />
+                <input type="text" className="w-full h-[60px] bg-bgcolor rounded-custom" />
             </div>
 
-            <div className="passwordInput">
+            <div className="passwordInput m-7">
                 <h1 className="m0 font-outfit text-primary font-medium">Password</h1>
-                <input type="text" className="w-75% bg-bgcolor rounded-custom"/>
+                <input type="text" className="w-full h-[60px] bg-bgcolor rounded-custom"/>
                 <h1 className="m0 font-outfit text-primary font-medium text-right">Forgot password?</h1>
             </div>
 
-            <div className="login">
-                <button className="bg-accent2 text-primary rounded-custom font-medium">
+            <div className="login m-7 flex flex-col items-center">
+                <button className="w-full h-[60px] bg-accent2 rounded-custom font-bold text-2xl">
                     Login
                 </button>
                 <h1 className="m0 font-outfit text-accent2 font-medium">Not registered? Create an account</h1>
             </div>
 
-            <div className="backgroundLogin opacity-100"></div>
+            <div className="backgroundLogin opacity-100 "></div>
         </div>
     )
 }
