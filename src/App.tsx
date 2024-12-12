205500cc3ba9bc35a9a875db8fa4df5398d9a5e2
ameleroparra <ameleroparra@gmail.de>
2024-12-12 11:19:26 +0100

    LoginPage edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import StickyProvider from "./context/Sticky";
-import MoviePage from "./pages/MoviePage";
+//import StickyProvider from "./context/Sticky";
+//import MoviePage from "./pages/MoviePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <StickyProvider content={<MoviePage />}></StickyProvider>;
+  //return <StickyProvider content={<MoviePage />}></StickyProvider>;
+  return <LoginPage />;
 }
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,1 +1,10 @@
 import "./css/LoginPage.css";
+
+const LoginPage = () => {
+    return (
+        <div className="grid-container max-w-full max-h-full rounded-custom m-5 bg-accent">
+            holi
+        </div>
+            )
+    }
+    export default LoginPage;
