45089f00c018954cd382f9a0eb1710aaffc6cf9e
taha <tkhapra@hof-university.de>
2024-12-18 14:30:08 +0100

    preloader added when loading pages. small css fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
 import StickyProvider from "./context/Sticky";
 import MoviePage from "./pages/MoviePage";
 import LoginPage from "./pages/LoginPage";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    const preloader = document.getElementById("preloader");
+    if (preloader) {
+      preloader.style.display = "none";
+    }
+  }, []);
+
   return (
     <Router>
       <StickyProvider
         content={
           <Routes>
-            <Route path="/" element={<MoviePage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
+            <Route path="/" element={<MoviePage />} />
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
         }
       />
     </Router>
   );
 }
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,48 +2,57 @@ import "./css/LoginPage.css";
 import nameWhite from "../assets/nameWhite.svg";
 
 const LoginPage = () => {
-    return (
-        <div className="login-grid-container max-w-full max-h-full rounded-custom bg-accent">
-            <div className="logo">
-                <img src={nameWhite} alt="Settima Logo" className="w-40 h-auto"/>
-            </div>
-
-            <div className="usernameInput m-7">
-                <h1 className="m0 font-outfit text-primary font-medium">
-                    Username / Email
-                </h1>
-                <input
-                    type="email"
-                    className="w-full h-[60px] bg-bgcolor rounded-custom p-3"
-                />
-            </div>
-
-            <div className="passwordInput m-7">
-                <h1 className="m0 font-outfit text-primary font-medium">Password</h1>
-                <input
-                    type="password"
-                    className="w-full h-[60px] bg-bgcolor rounded-custom p-3"
-                />
-                <h1 className="m0 font-outfit text-primary font-medium text-right">
-                    Forgot password?
-                </h1>
-            </div>
-
-            <div className="login m-7 flex flex-col items-center">
-                <button className="w-full h-[60px] bg-accent2 rounded-custom text-primary font-bold text-2xl">
-                    Login
-                </button>
-                <h1 className="m0 font-outfit text-accent2 font-medium">
-                    Not registered? Create an account
-                </h1>
-            </div>
-
-            <div className="backgroundLogin flex items-center justify-center">
-                <h1 className="text-primary font-bold text-3xl text-center">
-                    Rate, review and showcase your movie journey <br/> <u> one scene at a time.</u>
-                </h1>
-            </div>
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="login-grid-container rounded-custom bg-accent">
+        <div className="logo">
+          <img
+            src={nameWhite}
+            alt="Settima Logo"
+            className="w-40 h-auto ml-4 "
+          />
         </div>
-    );
+
+        <div className="usernameInput m-7">
+          <h1 className="m0 font-outfit text-primary font-medium p-2">
+            Username / Email
+          </h1>
+          <input
+            type="email"
+            className="w-full h-[60px] bg-bgcolor rounded-custom p-2"
+          />
+        </div>
+
+        <div className="passwordInput m-7">
+          <h1 className="m0 font-outfit text-primary font-medium p-2">
+            Password
+          </h1>
+          <input
+            type="password"
+            className="w-full h-[60px] bg-bgcolor rounded-custom p-2"
+          />
+          <h1 className="m0 font-outfit text-primary font-medium text-right p-2">
+            Forgot password?
+          </h1>
+        </div>
+
+        <div className="login m-7 flex flex-col items-center">
+          <button className="w-full h-[60px] bg-accent2 rounded-custom text-primary font-bold text-2xl">
+            Login
+          </button>
+          <h1 className="m0 font-outfit text-accent2 font-medium p-2">
+            Not registered? Create an account
+          </h1>
+        </div>
+
+        <div className="backgroundLogin flex items-center justify-center">
+          <h1 className="text-primary font-bold text-3xl text-center">
+            Rate, review and showcase your movie journey <br />{" "}
+            <u> one scene at a time.</u>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LoginPage;
