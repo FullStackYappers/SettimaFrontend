b602c948ec89301def1f4f0267d12f7a339beed8
taha <tkhapra@hof-university.de>
2024-12-28 19:02:20 +0100

    tested connection with backend

--- src/components/HomePageComponents/MovieCarousel.tsx
+++ src/components/HomePageComponents/MovieCarousel.tsx
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+interface Movie {
+  id: number;
+  title: string;
+  release_year: number;
+  runtime: number;
+  description: string;
+  rate_avg: number;
+  imageUrl?: string;
+}
+
+//dont ask me what any of this means, not for now atleast.
 const MovieCarousel = () => {
-  const movies = Array.from({ length: 20 }, (_, index) => ({
-    id: index + 1,
-    title: `Movie Title ${index + 1}`,
-    imageUrl: `src/assets/movieImg.jpg`,
-  }));
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/movies")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data: Movie[]) => {
+        setMovies(data.slice(0, 12));
+      })
+      .catch((error: Error) => {
+        console.error("Error fetching data:", error.message);
+      });
+  }, []);
 
   return (
     //daisyUI element. still need to implement arrows to navigate
     <div className="movieslist carousel carousel-center flex gap-8">
       {movies.map((movie) => (
         <div className="carousel-item flex flex-col items-center justify-center">
-          <Link to="/movie">
+          <Link to={`/movie/${movie.id}`}>
             <img src={movie.imageUrl} alt={movie.title} />
           </Link>
           <h3 className="movie-title">{movie.title}</h3>
