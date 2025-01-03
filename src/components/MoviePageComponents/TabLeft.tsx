bcca7699e3b7288378dfd030da369137f2469bb4
taha <tkhapra@hof-university.de>
2025-01-03 02:27:40 +0100

    api calls for crew added

--- src/components/MoviePageComponents/TabLeft.tsx
+++ src/components/MoviePageComponents/TabLeft.tsx
@@ -1,7 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface CrewMember {
+  person: { name: string };
+  department: { name: string };
+}
 
 const TabLeft = () => {
   const [activeTab, setActiveTab] = useState<string>("tab1");
+  const { movieId } = useParams();
+  const [crew, setCrew] = useState<CrewMember[]>([]);
+
+  useEffect(() => {
+    const fetchCrew = async () => {
+      console.log("api called");
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/movies/${movieId}}/crew`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCrew(data);
+      } catch (error: any) {
+        console.error("Error fetching data:", error.message as Error);
+      }
+    };
+
+    fetchCrew();
+  }, [movieId]);
+
+  const getCrewByDepartment = (department: string): JSX.Element[] => {
+    const members = crew
+      .filter((member) => member.department.name === department)
+      .map((member) => member.person.name);
+
+    {
+      /*using truthy and falsy js thing*/
+    }
+    return members.length > 0
+      ? [
+          <p>
+            <strong>{department}</strong>:{" "}
+            {members.map((name, index) => (
+              <span className="hover:text-accent2">
+                {name}
+                {index < members.length - 1 && ", "}
+                {/* */}
+              </span>
+            ))}
+          </p>,
+        ]
+      : [
+          <p>
+            <strong>{department}</strong> N/A
+          </p>,
+        ];
+  };
 
   return (
     <div className="tab-section-left overflow-visible bg-secondary rounded-custom w-full">
@@ -34,21 +90,24 @@ const TabLeft = () => {
 
       <div className="mt-4 font-semibold">
         {activeTab === "tab1" && (
-          <div id="tab1" className="tab-content block mb-4">
+          <div id="tab1" className="tab-content block mb-4 grid gap-4">
             <p>Actor 1</p>
             <p>Actor 2</p>
             <p>Actor 3</p>
           </div>
         )}
         {activeTab === "tab2" && (
-          <div id="tab2" className="tab-content block mb-4">
-            <p>Crew Member 1</p>
-            <p>Crew Member 2</p>
-            <p>Crew Member 3</p>
+          <div id="tab2" className="tab-content block mb-4 grid gap-4">
+            <p>{getCrewByDepartment("Director")}</p>
+            <p>{getCrewByDepartment("Writer")}</p>
+            <p>{getCrewByDepartment("Composer")}</p>
+            <p>{getCrewByDepartment("Editor")}</p>
+            <p>{getCrewByDepartment("Cinematographer")}</p>
+            <p>{getCrewByDepartment("Costume Designer")}</p>
           </div>
         )}
         {activeTab === "tab3" && (
-          <div id="tab3" className="tab-content block mb-4">
+          <div id="tab3" className="tab-content block mb-4 grid gap-4">
             <p>Location</p>
             <p>Release Date</p>
             <p>Language</p>
