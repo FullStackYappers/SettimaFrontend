6f9c62ec850257c7ec5517975b0de0450290d43f
taha <taha@uni.com>
2025-01-07 17:26:46 +0100

    cast fetching added

--- src/components/MoviePageComponents/TabLeft.tsx
+++ src/components/MoviePageComponents/TabLeft.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface CrewMember {
-  person: { name: string };
+  person: { name: string; id: number };
   department: { name: string };
+}
+
+interface CastMember {
+  person: { name: string; id: number };
+  character_name: string;
 }
 
 const TabLeft = () => {
   const [activeTab, setActiveTab] = useState<string>("tab1");
   const { movieId } = useParams();
   const [crew, setCrew] = useState<CrewMember[]>([]);
+  const [cast, setCast] = useState<CastMember[]>([]);
 
   useEffect(() => {
     const fetchCrew = async () => {
       console.log("api called");
       try {
         const response = await fetch(
-          `http://localhost:8000/api/movies/${movieId}}/crew`
+          `http://localhost:8000/api/movies/${movieId}/crew`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -28,13 +35,29 @@ const TabLeft = () => {
       }
     };
 
+    const fetchCast = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/movies/${movieId}/cast`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCast(data);
+      } catch (error: any) {
+        console.error("Error fetching data:", error.message as Error);
+      }
+    };
+
     fetchCrew();
+    fetchCast();
   }, [movieId]);
 
   const getCrewByDepartment = (department: string): JSX.Element[] => {
-    const members = crew
-      .filter((member) => member.department.name === department)
-      .map((member) => member.person.name);
+    const members = crew.filter(
+      (member) => member.department.name === department
+    );
 
     {
       /*using truthy and falsy js thing*/
@@ -42,13 +65,15 @@ const TabLeft = () => {
     return members.length > 0
       ? [
           <p>
-            <strong>{department}</strong>:{" "}
-            {members.map((name, index) => (
-              <span className="hover:text-accent2">
-                {name}
-                {index < members.length - 1 && ", "}
-                {/* */}
-              </span>
+            <span className="font-extrabold">{department}</span>:{" "}
+            {members.map((member, index) => (
+              <Link to={`/person/${member.person.id}`}>
+                <span className="hover:text-accent2">
+                  {member.person.name}
+                  {index < members.length - 1 && ", "}
+                  {/* */}
+                </span>
+              </Link>
             ))}
           </p>,
         ]
@@ -57,6 +82,21 @@ const TabLeft = () => {
             <strong>{department}</strong> N/A
           </p>,
         ];
+  };
+
+  const getCast = (): JSX.Element[] => {
+    return cast.length > 0
+      ? cast.map((member) => {
+          return (
+            <p>
+              <span className="font-extrabold">{member.character_name}</span>:{" "}
+              <Link to={`/person/${member.person.id}`}>
+                <span className="hover:text-accent2">{member.person.name}</span>
+              </Link>
+            </p>
+          );
+        })
+      : [<p>No cast information available</p>];
   };
 
   return (
@@ -91,9 +131,7 @@ const TabLeft = () => {
       <div className="mt-4 font-semibold">
         {activeTab === "tab1" && (
           <div id="tab1" className="tab-content block mb-4 grid gap-4">
-            <p>Actor 1</p>
-            <p>Actor 2</p>
-            <p>Actor 3</p>
+            {getCast()}
           </div>
         )}
         {activeTab === "tab2" && (
