451570e3dc81dd3d2319e37dafdfdd078cfb43ab
Antonio Melero Parra <ameleroparra@hof-university.de>
2025-01-19 16:45:09 +0100

    Fix Register Page, Bio feature started

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -49,16 +49,10 @@ const RegisterPage = () => {
     return (
         <div className="flex justify-center items-center w-screen h-screen">
             <div className="register-grid-container rounded-custom bg-accent">
-                <div className="logo">
-                    <img
-                        src={nameWhite}
-                        alt="Settima Logo"
-                        className="w-40 h-auto ml-4 "
-                    />
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="usernameInput m-4">
-                        <h1 className="m0 font-outfit text-primary font-medium p-2">
+
+                <form onSubmit={handleSubmit} className="register-form">
+                    <div className="usernameInput">
+                        <h1 className="text-big m0 font-outfit text-primary font-medium">
                             Username
                         </h1>
                         <input
@@ -66,12 +60,12 @@ const RegisterPage = () => {
                             name="username"
                             value={formData.username}
                             onChange={handleChange}
-                            className="w-full h-[60px] bg-bgcolor rounded-custom p-4"
+                            className="input-field bg-bgcolor rounded-custom p-4"
                         />
                     </div>
 
-                    <div className="emailInput m-4">
-                        <h1 className="m0 font-outfit text-primary font-medium p-2">
+                    <div className="emailInput">
+                        <h1 className="text-big m0 font-outfit text-primary font-medium">
                             Email
                         </h1>
                         <input
@@ -79,12 +73,12 @@ const RegisterPage = () => {
                             name="email"
                             value={formData.email}
                             onChange={handleChange}
-                            className="w-full h-[60px] bg-bgcolor rounded-custom p-4"
+                            className="input-field bg-bgcolor rounded-custom p-4"
                         />
                     </div>
 
-                    <div className="passwordInput m-4">
-                        <h1 className="m0 font-outfit text-primary font-medium p-2">
+                    <div className="passwordInput">
+                        <h1 className="text-big m0 font-outfit text-primary font-medium">
                             Password
                         </h1>
                         <input
@@ -92,12 +86,12 @@ const RegisterPage = () => {
                             name="password"
                             value={formData.password}
                             onChange={handleChange}
-                            className="w-full h-[60px] bg-bgcolor rounded-custom p-4"
+                            className="input-field w-full h-[60px] bg-bgcolor rounded-custom p-4"
                         />
                     </div>
 
-                    <div className="passwordConfirmInput m-4">
-                        <h1 className="m0 font-outfit text-primary font-medium p-2">
+                    <div className="passwordConfirmInput">
+                        <h1 className="text-big m0 font-outfit text-primary font-medium">
                             Confirm Password
                         </h1>
                         <input
@@ -105,12 +99,12 @@ const RegisterPage = () => {
                             name="password_confirmation"
                             value={formData.password_confirmation}
                             onChange={handleChange}
-                            className="w-full h-[60px] bg-bgcolor rounded-custom p-4"
+                            className="input-field w-full h-[60px] bg-bgcolor rounded-custom p-4"
                         />
                     </div>
 
-                    <div className="register m-4 flex flex-col items-center">
-                        <button type="submit" className="w-full h-[60px] bg-accent2 rounded-custom text-primary font-bold text-2xl">
+                    <div className="register flex flex-col items-center">
+                        <button type="submit" className="input-field bg-accent2 rounded-custom text-primary font-bold text-2xl">
                             Register
                         </button>
                     </div>
@@ -120,14 +114,24 @@ const RegisterPage = () => {
                 {successMessage && <div className="success-message text-green-500 text-center mt-4">{successMessage}</div>}
 
 
-                <div className="backgroundLogin flex items-center justify-center">
-                        <h1 className="text-primary font-bold text-3xl text-center">
-                            Start to share your favourite moments <br/>
-                            <u> one scene at a time.</u>
-                        </h1>
+                <div className="backgroundLogin flex flex-col items-center justify-center">
+                    <img
+                        src={nameWhite}
+                        alt="Settima Logo"
+                        className="w-4/5 h-auto ml-4"
+                    />
+                    <h1 className="text-giant text-primary font-bold text-center">
+                        Start to share your favourite moments <br/>
+                        <u> one scene at a time.</u>
+                    </h1>
+                    <div className="bg-accent w-11/12 h-[60]">
+                        <h1 className="text-big text-primary font-bold font-outfit">Tell us about yourself</h1>
+                        <input className="input-field bg-bgcolor rounded-custom p-4"/>
+
                     </div>
+                </div>
             </div>
         </div>
-);
+    );
 };
 export default RegisterPage;
