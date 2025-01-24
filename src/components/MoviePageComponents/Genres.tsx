fca8c54b11f1769bb26eddc7126fffe376429b3a
taha <tkhapra@hof-university.de>
2025-01-24 18:10:55 +0100

    small changes to genres in movie page

--- src/components/MoviePageComponents/Genres.tsx
+++ src/components/MoviePageComponents/Genres.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 //rounded-custom bg-secondary w-full h-full flex justify-center items-center mx-2
 
 const Genres = () => {
   const { movieId } = useParams();
-  const [genres, setGenres] = useState([]);
+  const [genres, setGenres] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchGenres = async () => {
@@ -22,21 +22,30 @@ const Genres = () => {
         console.error("Error fetching data:", error.message as Error);
       }
     };
-
     fetchGenres();
   }, [movieId]);
 
   return (
     <div className="genres text-base sm:text-xl lg:text-2xl font-semibold">
       {genres[0] && (
-        <div className="genre-box rounded-custom bg-secondary w-full h-full flex justify-center items-center mx-2">
-          {genres[0]}
-        </div>
+        <Link
+          to={`/movies/genres/${genres[0].toLowerCase()}`}
+          className="w-full mx-2"
+        >
+          <div className="genre-box rounded-custom bg-secondary w-full h-full flex justify-center items-center mx-2 hover:text-accent2">
+            {genres[0]}
+          </div>
+        </Link>
       )}
       {genres[1] && (
-        <div className="genre-box rounded-custom bg-secondary w-full h-full flex justify-center items-center mx-2">
-          {genres[1]}
-        </div>
+        <Link
+          to={`/movies/genres/${genres[1].toLowerCase()}`}
+          className="w-full mx-2"
+        >
+          <div className="genre-box rounded-custom bg-secondary w-full h-full flex justify-center items-center mx-2 hover:text-accent2">
+            {genres[1]}
+          </div>
+        </Link>
       )}
     </div>
   );
